0cb6f4affc8a0fff04bf4a2fd8b3c0ec20f150ba
vijaykyr <vijaykyr@gmail.com>
2016-11-05 19:14:37 -0400

    init

--- express_server.js
+++ express_server.js
@@ -20,7 +20,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.status(200).send('Hello, world!');
+  res.status(200).send('Hello, nodejs!');
 });
 
 // Start the server
