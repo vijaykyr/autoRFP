341bfa0f39c56ba820b5beb3358b60231a3abb69
vijaykyr <vijaykyr@gmail.com>
2016-11-06 15:14:05 -0500

    init

--- backend_service/appengine_config.py
+++
@@ -1,6 +0,0 @@
-#https://cloud.google.com/appengine/docs/python/tools/using-libraries-python-27
-# appengine_config.py
-from google.appengine.ext import vendor
-
-# Add any libraries install in the "lib" folder.
-vendor.add('lib')
