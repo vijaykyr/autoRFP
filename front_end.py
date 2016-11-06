00de4634fb50a4c583a6f7acbdc36c527840b401
vijaykyr <vijaykyr@gmail.com>
2016-11-06 16:22:13 -0500

    webapp2 works!

---
+++ front_end.py
@@ -0,0 +1,16 @@
+import webapp2
+
+class HelloWebapp2(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('Hello, webapp2!')
+
+app = webapp2.WSGIApplication([
+    ('/', HelloWebapp2),
+], debug=True)
+
+def main():
+    from paste import httpserver
+    httpserver.serve(app, host='127.0.0.1', port='8080')
+
+if __name__ == '__main__':
+    main()
